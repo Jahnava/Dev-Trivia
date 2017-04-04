86bf2cc93d5eb7556ff396cef6e439063f7281f4
jahnava <jahnavalmt@gmail.com>
2017-04-04 16:01:59 -0400

    Wrote a Learning Portal with interactive links.

--- public/js/learning.js
+++ public/js/learning.js
@@ -1,12 +1,24 @@
 var quote=[
-  'Failure is the mother of success.——Thomas Paine',
-  'The unexamined life is not worth living.——Socrates',
-  'None is of freedom or of life deserving unless he daily conquers it anew.——Erasmus',
-  'Our destiny offers not the cup of despair, but the chalice of opportunity. So let us seize it, not in fear, but in gladness.——R.M. Nixon',
-  'Living without an aim is like sailing without a compass.——John Ruskin'
+  'Make it simple. Make it memorable. Make it inviting to look at. Make it fun to read.   ——Leo Burnett',
+
+  'Inspiration exists, but it has to find us working.   ——Pablo Picasso',
+
+  'Just dont give up trying to do what you really want to do. Where there is love and inspiration, I dont think you can go wrong.   ——Ella Fitzgerald',
+
+  'Carpe Diem',
+
+  'Living without an aim is like sailing without a compass.   ——John Ruskin.',
+
+  'Every great dream begins with a dreamer. Always remember, you have within you the strength, the patience, and the passion to reach for the stars to change the world.   ——Harriet Tubman.'
+
 ];
 
 function newQuote(){
- var randomNumber=Math.floor(Math.random()*(quote.length));
- document.getElementById('quoteDisplay').innerHTML=quote[randomNumber];
+ var randomNumber = Math.floor(Math.random()*(quote.length));
+ document.getElementById('quoteDisplay').innerHTML='<br><h4>'+ quote[randomNumber] + '<br></h4>';
 }
+// 
+// function randomImage(){
+//   var myURL = 'http://lorempixel.com/400/200/business/';
+//   return myURL;
+// }
