16a78da8d3d3d7314a1e17bf06f02c868a329194
jahnava <jahnavalmt@gmail.com>
2017-04-06 23:14:54 -0400

    Did a bit cleaning in index.js

--- index.js
+++ index.js
@@ -1,21 +1,9 @@
 var express = require('express');
 var server = express();
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-
-var userRouter = require('./routers/user.router.js');
-var postRouter = require('./routers/post.router.js');
 
 var port = process.env.PORT || 8080;
-var mongoURI = process.env.MONGOURI || require('./secrets').MONGOURI;
 
 server.use(express.static(__dirname + '/public'));
-server.use(bodyParser.json());
-server.use(bodyParser.urlencoded({extended: true}));
-
-mongoose.connect(mongoURI);
-server.use(userRouter);
-server.use(postRouter);
 
 server.get('/', function(req, res){
   res.sendFile('index.html', {root: __dirname + '/public/html'});
