d51fdd60e42e8783292156fa3893f99dce60fe1a
jahnava <jahnavalmt@gmail.com>
2017-04-08 00:00:37 -0400

    Started styling my home page with a functional nav bar, sliders and gify background.

--- public/js/blog.js
+++
@@ -1,9 +0,0 @@
-
-  DiscourseEmbed = { discourseUrl: 'https://forums.digitalpoint.com/forums/programming.17/',
-                     discourseEmbedUrl: 'https://forums.digitalpoint.com/threads/still-cant-understand-js-whats-the-purpose-of-it.2796591/' };
-
-  (function() {
-    var d = document.createElement('script'); d.type = 'text/javascript'; d.async = true;
-    d.src = DiscourseEmbed.discourseUrl + 'javascripts/embed.js';
-    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(d);
-  })();
